bda55e4566d1a5eeab5473e39a37939967babf00
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-10-30 01:34:58 -0300

    Hotjar scripts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Hotjar from '@hotjar/browser';
 
-
+// Inicializa o Hotjar
+const siteId = process.env.REACT_APP_HOTJAR_SITE_ID;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
